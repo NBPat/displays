79eb39049e326a2d030f1e231b8a0ba350d6da9d
NBPat <50875140+NBPat@users.noreply.github.com>
2023-04-04 10:38:38 -0300

    Create demoC.c
    demo of various functions that toggle IOs on PORTB
    avr-gcc

---
+++ demoC.c
@@ -0,0 +1,42 @@
+#define reps 3
+#include <avr/io.h>
+#include <util/delay.h>
+int main() {
+  // decimal version of PORTB = 1 then PORTB = 0
+  PORTB = 1;       _delay_ms(500);        // led on
+  PORTB = 0;         _delay_ms(500);      // led off
+  // decimal version of PORTB = 255 then PORTB = 0
+  PORTB = 255;       _delay_ms(500);  
+  PORTB = 0;         _delay_ms(500);
+  // binary version of PORTB = 1 then PORTB = 0
+  PORTB = 0b00000001;    _delay_ms(500);  // led on
+  PORTB = 0b00000000;    _delay_ms(500);  // led off  
+  // binary version of PORTB = 255 then PORTB = 0 
+  PORTB = 0b11111111;    _delay_ms(500);  
+  PORTB = 0b00000000;    _delay_ms(500);  
+  // hexadecimal version of PORTB = 1 then PORTB = 0
+  PORTB = 0x01;      _delay_ms(500);      // led on
+  PORTB = 0x00;      _delay_ms(500);      // led off  
+  // hexadecimal version of PORTB = 255 then PORTB = 0
+  PORTB = 0xFF;      _delay_ms(500);      // led on
+  PORTB = 0x00;      _delay_ms(500);      // led off  
+    
+  // shifts single bit from PORTB.0 to PORTB.7
+  for (int j=0;j < reps;j++) {      
+  for (int i=0; i < 8 ;i++) { PORTB = (1<<i); _delay_ms(400);}
+  for (int i=0; i < 8 ;i++) { PORTB = (128>>i); _delay_ms(400);}
+}
+  // masking of bits with |= and &= operators
+  for (int j=0;j < reps;j++) {
+  PORTB |= 0b00000001;  _delay_ms(500);   // mask to force PORTB.0 = 1
+  PORTB &= 0b11111110;  _delay_ms(500);   // mask to force PORTB.0 = 0
+  }
+  // alternate pattern
+  for (int j=0;j < reps;j++) {
+  PORTB = 0x55;  _delay_ms(900);          // PORTB = 0b01010101
+  PORTB = 0xAA;   _delay_ms(900);         // PORTB = 0b10101010
+  }
+  // mathematical inverse of PORTB is put into PORTB  
+  for (int j=0;j < reps;j++) {
+  PORTB = !PORTB; _delay_ms(950);
+    }}
